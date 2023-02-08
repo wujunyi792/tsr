c74572f70ed50ce3f9dbd361de287ddfd5d92749
wujunyi <wu65830600@163.com>
2023-02-09 04:58:27 +0800

    chore: update

--- Test.py
+++ Test.py
@@ -9,23 +9,3 @@ basePath = "Dataset_2"
 if __name__ == '__main__':
 
     model = torch.load('model.mdl')
-
-    model.eval()
-
-    with open(path.join(basePath, "Train.csv")) as f:
-        lines = csv.reader(f)
-        title = True
-        for line in lines:
-            if title:
-                title = False
-                continue
-            img = cv2.imread(path.join(basePath, line[7]))
-            x = torch.tensor(img, dtype=torch.float)
-            x = x.ravel()
-            outputs = model(x)
-            _, predicted = torch.max(outputs, 1)
-            print(predicted)
-
-
-
-
